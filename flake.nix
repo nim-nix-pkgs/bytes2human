a55d45ab9e3ec0f344689c9954cdf6a2c7e2a967
hugosenari <hugosenari@gmail.com>
2022-03-12 00:57:47 -0300

    chore: re index bytes2human

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Convert bytes to kilobytes, megabytes, gigabytes, etc.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."bytes2human-master".url = "path:./master";
+  inputs."bytes2human-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bytes2human-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
