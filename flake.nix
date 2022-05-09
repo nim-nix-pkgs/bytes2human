88523f0f4b04d630373832f0a73244b67c87f63d
hugosenari <hugosenari@gmail.com>
2022-05-09 20:35:48 -0300

    chore: re index bytes2human

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."bytes2human-master".dir   = "master";
-  inputs."bytes2human-master".owner = "nim-nix-pkgs";
-  inputs."bytes2human-master".ref   = "master";
-  inputs."bytes2human-master".repo  = "bytes2human";
-  inputs."bytes2human-master".type  = "github";
-  inputs."bytes2human-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."bytes2human-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
